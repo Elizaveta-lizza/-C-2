15dc5fa64909c0012bc02eba3f5bd0783bed360f
Liza <kuznecovaliza11@yandex.ru>
2023-02-15 03:16:50 +0700

    Задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,32 @@
+﻿bool CheckValue(int Value)
+{
+    if (Value >= 100)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int GetThirdNumb(int Value)
+{
+    while (Value > 999)
+    {
+        Value /= 10;
+    }
+    return Value % 10;
+}
+
+Console.WriteLine("Введите  число:");
+int Value = Convert.ToInt32(Console.ReadLine());
+if (CheckValue(Value) == true)
+{
+    Console.WriteLine(GetThirdNumb(Value));
+
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+};

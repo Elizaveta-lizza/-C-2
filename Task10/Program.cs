590fea4556a89397fe0acd75e741752a247c0b3c
Liza <kuznecovaliza11@yandex.ru>
2023-02-15 01:50:08 +0700

    Задача 10

---
+++ Task10/Program.cs
@@ -0,0 +1,28 @@
+﻿bool CheckValue(int Value)
+{
+    if (Value >= 100 && Value <= 999)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int GetSecondNumb(int Value)
+{
+    int result = Value / 10;
+    return result % 10;
+}
+
+Console.WriteLine("Введите трехзначное число:");
+int Value = Convert.ToInt32(Console.ReadLine());
+if (CheckValue(Value) == true)
+{
+    Console.WriteLine(GetSecondNumb(Value));
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+};

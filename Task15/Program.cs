3ec3f2dd9e243ebcfb57ee00fa6d063e19e15540
Liza <kuznecovaliza11@yandex.ru>
2023-02-18 00:20:15 +0700

    Сделали 15 задачу

---
+++ Task15/Program.cs
@@ -0,0 +1,42 @@
+﻿bool CheckDay(int Day)
+{
+    if (Day >= 1 && Day <= 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool CheckDayOff(int Day)
+{
+    if (Day >= 6 && Day <= 7)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine("Введите  день недели:");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (CheckDay(Day) == true)
+{
+    if (CheckDayOff(Day) == true)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("Введен не день недели");
+};
+
